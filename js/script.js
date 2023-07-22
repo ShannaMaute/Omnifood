a8862f9be25abfdf42946aa25369e739ad24488e
Shanna Maute <shanna.maute@gmail.com>
2023-07-22 18:19:08 -0400

    Complete smooth scrolling

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function checkFlexGap() {
   var flex = document.createElement('div');
   flex.style.display = 'flex';
   flex.style.flexDirection = "column";
-  flex.style.rowGap = "1px";
+  flex.style.rowGap = '1px';
 
   flex.appendChild(document.createElement('div'));
   flex.appendChild(document.createElement('div'));
@@ -18,6 +18,32 @@ function checkFlexGap() {
 }
 
 checkFlexGap();
+
+// Smooth Scrolling Animation
+
+const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const href = link.getAttribute('href');
+
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    // Scroll to other links.
+    if (href !== "#" && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation.
+    if (link.classList.contains('main-nav-link'))
+      headerEl.classList.toggle('nav-open');
+  });
+});
 
 // Set Current Year
 
